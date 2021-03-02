83fe4c106738993f79504b21ddef9be6de4b2f6c
jerem <llanta.jeremy@gmail.com>
2021-03-02 10:39:33 +0100

    post register

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('views'));
+
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.post('/register', (req, res) => {
+	const { firstname, lastname, birthdate, address, mail, phone } = req.body;
+	if (!firstname || !lastname || !birthdate || !address || !mail || !phone)
+		res.status(403).json('missing body parameters');
+
+	res.status(201).json('It works');
 });
 
 app.listen(port, () => {
