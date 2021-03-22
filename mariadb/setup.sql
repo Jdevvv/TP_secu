ad0e2fe50f806ff82fdbf943bf776648699e472a
Ayce45 <evan.j@hotmail.fr>
2021-03-22 22:41:13 +0100

    ✨ create table user ✨ https

--- mariadb/setup.sql
+++ mariadb/setup.sql
@@ -3,3 +3,14 @@ CREATE USER secu IDENTIFIED BY 'MYSQL_SECU_PASSWORD';
 GRANT USAGE ON *.* TO 'secu'@'%' IDENTIFIED BY 'MYSQL_SECU_PASSWORD';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `secu`.* TO secu;
 FLUSH PRIVILEGES;
+CREATE TABLE `user`
+(
+    id        int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    firstname varchar(35)  NOT NULL,
+    lastname  varchar(35)  NOT NULL,
+    birthdate date         NOT NULL,
+    email     varchar(255) NOT NULL,
+    phone     varchar(15)  NOT NULL,
+    address   varchar(95)  NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
